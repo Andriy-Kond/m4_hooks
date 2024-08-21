facfc4b416feefab2501912b91b2f76183a4d2eb
Andrii <andrii.kondratovich@gmail.com>
2024-08-21 12:57:56 +0300

    add: main styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
 import { GlobalStyle } from "GlobalStyle";
 
-// import ColorPicker from "ColorPicker";
-// import Pokemon from "Pokemon";
-// import Todo from "Todo";
-// import Tabs from "Tabs";
-// import Clock from "Clock";
-// import LoginForm from "LoginForm";
-// import Materials from "Materials";
-// import Publications from "Publications";
+import ColorPicker from "ColorPicker";
+import Pokemon from "Pokemon";
+import Todo from "Todo";
+import Tabs from "Tabs";
+import Clock from "Clock";
+import LoginForm from "LoginForm";
+import Materials from "Materials";
+import Publications from "Publications";
 import VideoPlayer from "VideoPlayer";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-
-      {/* <ColorPicker /> */}
-      {/* <Pokemon /> */}
-      {/* <Todo /> */}
-      {/* <Tabs /> */}
-      {/* <Clock /> */}
-      {/* <LoginForm /> */}
-      {/* <Materials /> */}
-      {/* <Publications /> */}
+      <br />
+      <hr />
+      <hr />
+      <ColorPicker />
+      <br />
+      <hr />
+      <hr />
+      <Pokemon />
+      <br />
+      <hr />
+      <hr />
+      <Todo />
+      <br />
+      <hr />
+      <hr />
+      <Tabs />
+      <br />
+      <hr />
+      <hr />
+      <Clock />
+      <br />
+      <hr />
+      <hr />
+      <LoginForm />
+      <br />
+      <hr />
+      <hr />
+      <Materials />
+      <br />
+      <hr />
+      <hr />
+      <Publications />
+      <br />
+      <hr />
+      <hr />
       <VideoPlayer />
     </>
   );
