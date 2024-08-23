e82387d82b5db65a1fbe88f5706a38411d376ac6
Andrii <andrii.kondratovich@gmail.com>
2024-08-23 20:33:31 +0300

    fix signupForm

--- src/SignupForm/SignupForm.jsx
+++ src/SignupForm/SignupForm.jsx
@@ -1,24 +1,41 @@
 import { useEffect, useState } from "react";
 import { Form, Label } from "./SignupForm.styled";
 
+// Хук для позбуття повторів
+const useLocalStorage = (localStorageKey, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem(localStorageKey)) ?? defaultValue
+    );
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [localStorageKey, state]);
+
+  return [state, setState];
+};
+
 function SignupForm() {
-  const [email, setEmail] = useState(() => {
-    const savedEmail = window.localStorage.getItem("signupFormEmail");
-    return savedEmail ? JSON.parse(savedEmail) : "";
-  });
+  // const [email, setEmail] = useState(() => {
+  //   const savedEmail = window.localStorage.getItem("signupFormEmail");
+  //   return savedEmail ? JSON.parse(savedEmail) : "";
+  // });
 
-  const [password, setPassword] = useState(() => {
-    const savedPassword = window.localStorage.getItem("signupFormPassword");
-    return savedPassword ? JSON.parse(savedPassword) : "";
-  });
+  // useEffect(() => {
+  //   window.localStorage.setItem("signupFormEmail", JSON.stringify(email));
+  // }, [email]);
+  const [email, setEmail] = useLocalStorage("signupFormEmail", "");
 
-  useEffect(() => {
-    window.localStorage.setItem("signupFormEmail", JSON.stringify(email));
-  }, [email]);
+  // const [password, setPassword] = useState(() => {
+  //   const savedPassword = window.localStorage.getItem("signupFormPassword");
+  //   return savedPassword ? JSON.parse(savedPassword) : "";
+  // });
 
-  useEffect(() => {
-    window.localStorage.setItem("signupFormPassword", JSON.stringify(password));
-  }, [password]);
+  // useEffect(() => {
+  //   window.localStorage.setItem("signupFormPassword", JSON.stringify(password));
+  // }, [password]);
+  const [password, setPassword] = useLocalStorage("signupFormPassword", "");
 
   const handleChange = evt => {
     const { name, value } = evt.target;
