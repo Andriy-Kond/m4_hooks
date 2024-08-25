4b362fd7ef22e1e3f4c1b37216b4b449f5f4700a
Andrii <andrii.kondratovich@gmail.com>
2024-08-25 20:26:02 +0300

    fix: news

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -47,7 +47,7 @@ function News() {
   }, [currentPage, searchQuery]);
 
   const updatePage = () => {
-    setCurrentPage(prevPage => prevPage + 1);
+    setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
   };
 
   const onChangeQuery = query => {
@@ -97,3 +97,86 @@ function News() {
 }
 
 export default News;
+
+// class NewsOld extends Component {
+//   state = {
+//     articles: [],
+//     currentPage: 1,
+//     searchQuery: "",
+//     isLoading: false,
+//     error: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.fetchArticles();
+//     }
+//   }
+
+//   onChangeQuery = query => {
+//     this.setState({
+//       searchQuery: query,
+//       currentPage: 1,
+//       articles: [],
+//       error: null,
+//     });
+//   };
+
+//   fetchArticles = () => {
+//     const { currentPage, searchQuery } = this.state;
+//     const options = { searchQuery, currentPage };
+
+//     this.setState({ isLoading: true });
+
+//     APIfetchArticles(options)
+//       .then(articles => {
+//         this.setState(prevState => ({
+//           articles: [...prevState.articles, ...articles],
+//           currentPage: prevState.currentPage + 1,
+//         }));
+//       })
+//       .catch(error => this.setState({ error }))
+//       .finally(() => this.setState({ isLoading: false }));
+//   };
+
+//   render() {
+//     const { articles, isLoading, error } = this.state;
+//     const shouldRenderLoadMoreButton = articles.length > 0 && !isLoading;
+
+//     return (
+//       <div>
+//         {error && <h1>Ой ошибка, всё пропало!!!</h1>}
+
+//         <NewsSearchForm onSubmit={this.onChangeQuery} />
+
+//         <ul>
+//           {articles.map(({ title, url }) => (
+//             <li key={title}>
+//               <a href={url} target="_blank" rel="noopener noreferrer">
+//                 {title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+
+//         {shouldRenderLoadMoreButton && (
+//           <button type="button" onClick={this.fetchArticles}>
+//             Загрузить ещё
+//           </button>
+//         )}
+
+//         {isLoading && (
+//           <p style={{ fontSize: 24, display: "flex", alignItems: "center" }}>
+//             Загружаем...
+//             <span
+//               aria-label="Иконка"
+//               role="img"
+//               style={{ fontSize: 32, marginLeft: 10 }}>
+//               🧙‍♂️
+//             </span>
+//           </p>
+//         )}
+//       </div>
+//     );
+//   }
+// }
